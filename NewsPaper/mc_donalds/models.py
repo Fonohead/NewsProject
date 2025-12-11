fbc3d1422b00da40788ba16612f2c3b582005b3b
Ed <44eduard44@gmail.com>
2025-12-11 21:00:07 +0200

    New update

--- NewsPaper/mc_donalds/models.py
+++ NewsPaper/mc_donalds/models.py
@@ -1,19 +1,45 @@
+from email.policy import default
+from random import choices
+
 from django.db import models
 
-class Order(models.Model):
-    pass
+director = 'DI'
+admin = 'AD'
+cook = 'CO'
+cashier = 'CA'
+cleaner = 'CL'
+
+POSITIONS = [
+    (director, "Директор"),
+    (admin, "Администратор"),
+    (cook, "Повар"),
+    (cashier, "Кассир"),
+    (cleaner, "Уборщик")
+]
+class Staff(models.Model):
+    full_name = models.CharField(max_length=255)
+    position = models.CharField(max_length=2, choices=POSITIONS, default=cashier)
+    labour_contract = models.IntegerField()
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField(default=0.0)
+    composition = models.TextField(default='Состав не указан')
 
-class Staff(models.Model):
-    full_name = models.CharField(max_length=255)
-    position = models.CharField(max_length=255)
-    labour_contract = models.IntegerField()
+class Order(models.Model):
+    time_in = models.DateTimeField(auto_now_add=True)
+    time_out = models.DateTimeField(null=True)
+    cost = models.FloatField(default=0.0)
+    pickup = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False)
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+
+    products = models.ManyToManyField(Product, through='ProductOrder')
 
 class ProductOrder(models.Model):
-    pass
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=1)
 
 
 
