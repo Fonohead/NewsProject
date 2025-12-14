4f5fe82233992d34f3a5b9c2a6a1d9c737e0fbf0
Ed <44eduard44@gmail.com>
2025-12-14 16:03:07 +0200

    Updated Sunday

--- NewsPaper/mc_donalds/models.py
+++ NewsPaper/mc_donalds/models.py
@@ -2,6 +2,8 @@ from email.policy import default
 from random import choices
 
 from django.db import models
+from datetime import datetime
+
 
 director = 'DI'
 admin = 'AD'
@@ -33,13 +35,36 @@ class Order(models.Model):
     pickup = models.BooleanField(default=False)
     complete = models.BooleanField(default=False)
     staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
-
     products = models.ManyToManyField(Product, through='ProductOrder')
+
+    def finish_order(self):
+        self.time_out = datetime.now()
+        self.complete = True
+        self.save()
+
+    def get_order_duration(self):
+        if self.complete:
+            return (self.time_out - self.time_in).total_seconds() // 60
+        else:
+            return (datetime.now() - self.time_in).total_seconds() // 60
+
 
 class ProductOrder(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    amount = models.IntegerField(default=1)
+    _amount = models.IntegerField(default=1, db_column='amount')
+
+    def order_summ(self):
+        return self.product.price * self.amount
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, value):
+        self._amount = int(value) if value >= 0 else 0
+        self.save()
 
 
 
